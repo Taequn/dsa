938e0ed155bd40b627c061c6e64554f76f1555dd
mminbay <mminbay@colgate.edu>
2022-02-11 17:00:18 -0500

    can now generate descriptors

--- Lab02/SemanticSimilarity.java
+++ Lab02/SemanticSimilarity.java
@@ -1,11 +1,66 @@
 import java.util.HashMap;
+import java.util.Map;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 class SemanticSimilarity {
-    HashMap<String, HashMap<String, Integer>> descriptors;
+    private HashMap<String, HashMap<String, Integer>> descriptors;
 
     public SemanticSimilarity(Iterable<String> sentences) {
-        //TO-DO: Implement constructor
+        descriptors = new HashMap<String, HashMap<String, Integer>>();
+        generateDescriptors(sentences);
+    }
+
+    private void generateDescriptors(Iterable<String> sentences) {
+        for (String s: sentences) {
+            ArrayList<String> words = sentenceToWords(s);
+            for (int i = 0; i < words.size() - 1; i++) {
+                for (int j = i + 1; j < words.size(); j++) {
+                    String w1 = words.get(i);
+                    String w2 = words.get(j);
+                    if (w1.equals(w2)) {
+                        continue;
+                    }
+                    if (!descriptors.containsKey(w1)) {
+                        descriptors.put(w1, new HashMap<String, Integer>());
+                    }
+                    if (!descriptors.containsKey(w2)) {
+                        descriptors.put(w2, new HashMap<String, Integer>());
+                    }
+
+                    HashMap<String, Integer> d1 = descriptors.get(w1);
+                    HashMap<String, Integer> d2 = descriptors.get(w2);
+
+                    if (!d1.containsKey(w2)) {
+                        d1.put(w2, 1);
+                    }
+                    else {
+                        d1.put(w2, d1.get(w2) + 1);
+                    }
+
+                    if (!d2.containsKey(w1)) {
+                        d2.put(w1, 1);
+                    }
+                    else {
+                        d2.put(w1, d2.get(w1) + 1);
+                    }
+                }
+            }
+        }
+    }
+
+    /***
+     * Tester method to print contents of descriptors
+     */
+    public void printDescriptors() {
+        for (Map.Entry<String, HashMap<String, Integer>> word: descriptors.entrySet()) {
+            System.out.println(word.getKey() + ":");
+            System.out.println("{");
+            for (Map.Entry<String, Integer> occurence: word.getValue().entrySet()) {
+                System.out.println("\t" + occurence.getKey() + ": " + occurence.getValue());
+            }
+            System.out.println("}");
+        }
     }
 
     /***
@@ -32,10 +87,27 @@ class SemanticSimilarity {
                 }
             }
         }
+        if (!currentWord.equals("")) {
+            result.add(currentWord.toLowerCase());
+        }
         return result;
     }
 
     public static void main(String[] args) {
+        /*
         System.out.println(sentenceToWords("Hello world i am very cool"));
+        System.out.println(sentenceToWords("This, is, -a- elaborately! punctuated? sentence?"));
+        System.out.println(sentenceToWords("Incorre;ctly punc1tuated !!!/12sentence13 with 123123 numbers"));
+        */
+        String[] test = {
+            "This is a very handsome sentence",
+            "This is a blue dog",
+            "The blue dog is very happy",
+            "The mannequin is mediocre",
+            "The dog said \"THIS IS A VERY HANDSOME BLUE MANNEQUIN\""
+        };
+
+        SemanticSimilarity testtest = new SemanticSimilarity(Arrays.asList(test));
+        testtest.printDescriptors();
     }
 }
