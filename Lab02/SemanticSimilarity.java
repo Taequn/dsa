aa8f512644d50e7e30a7e3571aaf187ef4cba260
Josiah <jopaintsil@gmail.com>
2022-02-12 13:18:12 -0500

    Implement equation. Remove Arraylist repitition.

--- Lab02/SemanticSimilarity.java
+++ Lab02/SemanticSimilarity.java
@@ -1,7 +1,8 @@
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.lang.Math;
 
 class SemanticSimilarity {
     private HashMap<String, HashMap<String, Integer>> descriptors;
@@ -13,11 +14,9 @@ class SemanticSimilarity {
 
     private void generateDescriptors(Iterable<String> sentences) {
         for (String s: sentences) {
-            ArrayList<String> words = sentenceToWords(s);
-            for (int i = 0; i < words.size() - 1; i++) {
-                for (int j = i + 1; j < words.size(); j++) {
-                    String w1 = words.get(i);
-                    String w2 = words.get(j);
+            HashSet<String> words = sentenceToWords(s);
+            for (String w1 : words) {
+                for (String w2 : words) {
                     if (w1.equals(w2)) {
                         continue;
                     }
@@ -29,20 +28,12 @@ class SemanticSimilarity {
                     }
 
                     HashMap<String, Integer> d1 = descriptors.get(w1);
-                    HashMap<String, Integer> d2 = descriptors.get(w2);
 
                     if (!d1.containsKey(w2)) {
                         d1.put(w2, 1);
                     }
                     else {
                         d1.put(w2, d1.get(w2) + 1);
-                    }
-
-                    if (!d2.containsKey(w1)) {
-                        d2.put(w1, 1);
-                    }
-                    else {
-                        d2.put(w1, d2.get(w1) + 1);
                     }
                 }
             }
@@ -69,8 +60,8 @@ class SemanticSimilarity {
      * @param sentence: the sentence to be processed
      * @return          the Arraylist containing the words
      */
-    private static ArrayList<String> sentenceToWords(String sentence) {
-        ArrayList<String> result = new ArrayList<String>();
+    private static HashSet<String> sentenceToWords(String sentence) {
+        HashSet<String> result = new HashSet<String>();
         String currentWord = "";
         for(int i = 0; i < sentence.length(); i++){
             char currentChar = sentence.charAt(i);
@@ -93,21 +84,63 @@ class SemanticSimilarity {
         return result;
     }
 
+    public double similarity(String w1, String w2) {
+        // Verification
+        w1 = w1.toLowerCase();
+        w2 = w2.toLowerCase();
+        if ( (!descriptors.containsKey(w1)) || (!descriptors.containsKey(w2)) ) {
+            return -1.0;
+        }
+
+        int numeratorSum = 0;
+        int aSquared = 0;
+        int bSquared = 0;
+
+        // Loop through each set of descriptors
+        for (String key : descriptors.get(w1).keySet()) {
+            if (descriptors.get(w2).containsKey(key)) {
+                numeratorSum += descriptors.get(w1).get(key) * descriptors.get(w2).get(key);
+                aSquared += descriptors.get(w1).get(key) * descriptors.get(w1).get(key);
+            } else {
+                aSquared += descriptors.get(w1).get(key) * descriptors.get(w1).get(key);
+            }
+        }
+
+        for (String key : descriptors.get(w2).keySet()) {
+            bSquared += descriptors.get(w2).get(key) * descriptors.get(w2).get(key);
+        }
+
+        return (numeratorSum/(Math.sqrt(aSquared) * Math.sqrt(bSquared)));
+    }
+
     public static void main(String[] args) {
         /*
         System.out.println(sentenceToWords("Hello world i am very cool"));
         System.out.println(sentenceToWords("This, is, -a- elaborately! punctuated? sentence?"));
         System.out.println(sentenceToWords("Incorre;ctly punc1tuated !!!/12sentence13 with 123123 numbers"));
         */
-        String[] test = {
-            "This is a very handsome sentence",
-            "This is a blue dog",
-            "The blue dog is very happy",
-            "The mannequin is mediocre",
-            "The dog said \"THIS IS A VERY HANDSOME BLUE MANNEQUIN\""
-        };
+        // String[] test = {
+        //     // "This is a very handsome sentence",
+        //     "This is a blue dog",
+        //     "This is a blue dog",
+        //     // "Blue is a blue Blue",
+        //       "dog dog dog dog dog is",
+        //     // "The blue dog is very happy",
+        //     // "The mannequin is mediocre",
+        //     // "The dog said \"THIS IS A VERY HANDSOME BLUE MANNEQUIN\""
+        // };
 
-        SemanticSimilarity testtest = new SemanticSimilarity(Arrays.asList(test));
-        testtest.printDescriptors();
+        // SemanticSimilarity testtest = new SemanticSimilarity(Arrays.asList(test));
+        // testtest.printDescriptors();
+
+        // System.out.println(testtest.similarity("dog", "dog"));
+        // System.out.println(testtest.similarity("dog", "is"));
+        // System.out.println(testtest.similarity("dog", "blue"));
+        // System.out.println(testtest.similarity("mannequin", "very"));
+        // System.out.println(testtest.similarity("dog", "The"));
+        // System.out.println(testtest.similarity("--", "is"));
+        // System.out.println(testtest.similarity(" ", "is"));
+
+
     }
 }
